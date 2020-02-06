b201c4ec72d63eea610e5a959d9636326f46d6b7
Mike Pattyn <mike@givtapp.net>
2020-02-06 13:21:39 +0100

    use axios interceptor

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -5,31 +5,40 @@ import { Logger } from "../logger";
 
 @injectable()
 export class ApiClient {
+
     private baseUrl: string | undefined = "http://localhost:3000/api/v1";
 
     constructor(@inject(Logger) private logger: Logger) {
-
+        // Add a request interceptor
+        axios.interceptors.request.use(function (config) {
+            if(!isNullOrUndefined(config) && !isNullOrUndefined(config.headers) && !isNullOrUndefined(config.headers.common)) {
+                var headers = config.headers.common;
+                if (isNullOrUndefined(headers.Accept)) {
+                    headers.Accept = "application/json"
+                }
+            }
+            // Do something before request is sent
+            console.log("Config: ",config)
+            return config;
+        }, function (error) {
+            // Do something with request error
+            return Promise.reject(error);
+        });
     }
 
     async post(url: string, headers: any, body: any) {
         var retVal = null
 
-        const config: AxiosRequestConfig = {
-            method: "POST",
-            url: `${this.baseUrl}${url}`
-        }
+        const config: AxiosRequestConfig = {}
 
+        if (!isNullOrUndefined(url))
+            config.url = `${this.baseUrl}${url}`
         if (!isNullOrUndefined(headers))
             config.headers = headers
-        else
-            config.headers = {
-                accept: "application/json"
-            }
-
         if (!isNullOrUndefined(body))
             config.data = body;
 
-        await axios(config)
+        axios.post(config.url!, config.data)
             .then((response: AxiosResponse) => {
                 console.log(response)
                 retVal = response
