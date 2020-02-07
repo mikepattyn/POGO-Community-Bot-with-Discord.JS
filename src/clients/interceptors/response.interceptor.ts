456c58d7d48a1e3b861fd64174a19ffe3ae7c09c
Mike Pattyn <mike@givtapp.net>
2020-02-07 18:29:31 +0100

    added seperate interceptors

---
+++ src/clients/interceptors/response.interceptor.ts
@@ -0,0 +1,7 @@
+import { AxiosResponse } from "axios"
+import { EnforceErrorGetsThrownWhenUnsuccessfulResponse } from "./functions/EnforceErrorGetsThrownWhenUnsuccessfulResponse"
+
+export const ResponseInterceptor = (response: AxiosResponse) => {
+    EnforceErrorGetsThrownWhenUnsuccessfulResponse(response)
+}
+
